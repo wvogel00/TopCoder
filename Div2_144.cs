ffdb609e9d124cac8c9c9c4e07f3c740c338af1d
torii <eagle.hokkaido2317@gmail.com>
2016-12-16 23:06:19 +0900

    Hard

--- Div2_144.cs
+++ Div2_144.cs
@@ -1,10 +1,42 @@
 ﻿using System;
-namespace bfs
+using System.Collections.Generic;
+
+namespace TopCoder
 {
-	public class Div2_144
+	namespace Div2_144
 	{
-		public Div2_144()
+		public class Outage
 		{
+			public int output(int[] fromJ, int[] toJ, int[] ductL)
+			{
+				int[] cost = new int[fromJ.Length + 1];
+				int maxL = 0, tweiL = 0;
+
+				foreach (int v in ductL)
+					tweiL += 2 * v;
+
+				Queue<int> queue = new Queue<int>();
+				queue.Enqueue(0);
+
+				while (queue.Count != 0)
+				{
+					for (int i = 0; i < fromJ.Length; i++)
+					{
+						if (queue.Contains(fromJ[i]))
+						{
+							queue.Enqueue(toJ[i]);
+							cost[toJ[i]] = cost[fromJ[i]] + ductL[i];
+						}
+					}
+					queue.Dequeue();
+				}
+
+				foreach (int v in cost)
+					maxL = maxL < v ? v : maxL;
+
+				return tweiL - maxL;
+			}
 		}
+	}
 	}
 }
