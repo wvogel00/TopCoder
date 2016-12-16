2e326e8fc3cb40486def401172372ef24a33c717
torii <eagle.hokkaido2317@gmail.com>
2016-12-16 23:08:39 +0900

    Div2 145 Easy

---
+++ Div2_145.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TopCoder
+{
+	namespace Div2_145
+	{
+		//easy
+		public class ImageDithering
+		{
+			public int count(string dithered, string[] screen)
+			{
+				char[] keys = dithered.ToCharArray();
+				int count = 0;
+				for (int i = 0; i < screen.Length; i++)
+				{
+					char[] sc2 = screen[i].ToCharArray();
+					for (int j = 0; j < sc2.Length; j++)
+					{
+						for (int k = 0; k < keys.Length; k++)
+						{
+							if (sc2[j] == keys[k])
+								count++;
+						}
+					}
+				}
+				return count;
+			}
+		}
+	}
+}
